6df72dff6ec7e1185dc0a916af6640ff33ac2747
钩钩么么哒 <a516550257@qq.com>
2015-12-24 20:21:50 +0800

    create the project

---
+++ Jin Jiajun/Jin JiajunTests/Jin_JiajunTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jin_JiajunTests.swift
+//  Jin JiajunTests
+//
+//  Created by 钩钩么么哒 on 15/12/24.
+//  Copyright © 2015年 钩钩么么哒. All rights reserved.
+//
+
+import XCTest
+@testable import Jin_Jiajun
+
+class Jin_JiajunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
